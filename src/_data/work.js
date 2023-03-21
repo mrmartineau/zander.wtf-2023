da5d8b4495600b23268808c169c54320fcc4d286
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-03-21 09:45:49 +0000

    🌅 Good morning

---
+++ src/_data/work.js
@@ -0,0 +1,64 @@
+module.exports = {
+  jobs: {
+    current: {
+      name: 'Fathom',
+      description: 'Cryptocurrency exchange',
+      type: 'contract',
+      url: 'https://www.fathomlondon.com/',
+    },
+    previous: [
+      {
+        name: 'Utopia Music',
+        description: 'Rostr 2.0',
+        type: 'contract',
+        url: 'https://utopiamusic.com/',
+      },
+      {
+        name: 'Babylon Health',
+        description: '',
+        type: 'contract',
+        url: 'https://babylonhealth.com',
+      },
+      {
+        name: 'Digital Futures',
+        description: '',
+        type: 'contract',
+        url: 'https://digitalfutures.com',
+      },
+      {
+        name: 'Heights',
+        description: '',
+        type: 'contract',
+        url: 'https://www.yourheights.com',
+      },
+      {
+        name: 'Curve',
+        description: '',
+        type: 'contract',
+        url: 'https://curve.com',
+      },
+      {
+        name: 'FairFX',
+        description: '',
+        type: 'contract',
+        url: 'https://fairfx.com',
+      },
+    ],
+  },
+  sideProjects: [
+    {
+      name: 'Otter',
+      description: 'Personal bookmarking app',
+    },
+    {
+      name: 'Code Notes',
+      description: '11ty theme for your digital garden',
+      repo: 'https://github.com/mrmartineau/notes.zander.wtf',
+    },
+    {
+      name: 'Gatsby Theme Code Notes',
+      description: 'Gatsby theme for your digital garden (archived)',
+      repo: 'https://github.com/mrmartineau/gatsby-theme-code-notes',
+    },
+  ],
+}
